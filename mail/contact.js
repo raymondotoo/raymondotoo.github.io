7fb53430d335c7a9859e8ac16b2a946086051946
Raymond Otoo <raotoo@miners.utep.edu>
2026-03-13 23:05:15 -0700

    updated site with form

--- mail/contact.js
+++ mail/contact.js
@@ -10,18 +10,20 @@ $(function () {
             var email = $("input#email").val();
             var subject = $("input#subject").val();
             var message = $("textarea#message").val();
+            var company = $("input#company").val();
 
             $this = $("#sendMessageButton");
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "contact.php",
+                url: "mail/contact.php",
                 type: "POST",
                 data: {
                     name: name,
                     email: email,
                     subject: subject,
-                    message: message
+                    message: message,
+                    company: company
                 },
                 cache: false,
                 success: function () {
@@ -38,7 +40,7 @@ $(function () {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
+                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems the mail server is not responding. Please try again later or reach out on LinkedIn."));
                     $('#success > .alert-danger').append('</div>');
                     $('#contactForm').trigger("reset");
                 },
--- mail/contact.php
+++ mail/contact.php
@@ -1,5 +1,10 @@
 <?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+if (empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
+
+if (!empty($_POST['company'])) {
   http_response_code(500);
   exit();
 }
@@ -12,9 +17,11 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = "raymondotoo115@gmail.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
+$headers = "From: $email\r\n";
+$headers .= "Reply-To: $email\r\n";
+$headers .= "X-Mailer: PHP/" . phpversion();
 
-if(!mail($to, $subject, $body, $header))
+if (!mail($to, $subject, $body, $headers)) {
   http_response_code(500);
+}
 ?>
